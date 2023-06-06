e4d6fc8e2ca83f79cc6b3de917693762268537ed
Totoblak90 <tobias.blaksley@hotmail.com>
2023-06-06 00:15:21 -0300

    agregue más propiedades a la interfaz de perros y cargue objetos a la api del mismo

--- src/app/interfaces/dog.interface.ts
+++ src/app/interfaces/dog.interface.ts
@@ -1,19 +1,29 @@
 export class Dog {
-  weight?:             Measurement;
-  height?:             Measurement;
-  id?:                 number;
-  name?:               string;
-  bred_for?:           string;
-  breed_group?:        string;
-  life_span?:          string;
-  temperament?:        string;
-  origin?:             string;
-  reference_image_id?: string;
-  image?:              Image;
-  searchTerms?:        string[];
-  country_code?:        string;
-  description?:         string;
-  history?:             string;
+  weight?:                                Measurement;
+  height?:                                Measurement;
+  id?:                                    number;
+  name?:                                  string;
+  bred_for?:                              string;
+  breed_group?:                           string;
+  life_span?:                             string;
+  temperament?:                           string;
+  origin?:                                string;
+  reference_image_id?:                    string;
+  image?:                                 Image;
+  searchTerms?:                           string[];
+  country_code?:                          string;
+  description?:                           string;
+  history?:                               string;
+  categoria_de_tamanio?:                  'Muy pequeño' | 'Pequeño' | 'Mediano' | 'Grande' | 'Muy grande';
+  perks?:                                 string[];
+  color?:                                 string;
+  nivel_de_energia?:                      'Bajo' | 'Moderado' | 'Alto';
+  facilidad_de_entrenamiento?:            'Bajo' | 'Moderado' | 'Alto';
+  salud_general?:                         'Excelente' | 'Buena' | 'Regular' | 'Mala';
+  compatibilidad_con_otros_animales?:     'Baja' | 'Moderada' | 'Alta';
+  necesidades_especiales?:                string[];
+  popularidad?:                           'Baja' | 'Moderada' | 'Alta';
+  enfermedades_comunes?:                  string[];
 
   constructor(data: any) {
     Object.assign(this, data);
