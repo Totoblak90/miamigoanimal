854e44eed6d82274cfa775a965db657106957346
Totoblak90 <tobias.blaksley@hotmail.com>
2023-06-09 01:14:14 -0300

    configured cookies management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Creo el tipado
+type googleScript = {
+  url: string,
+  async: boolean,
+  innerHtml: string,
+  crossOrigin: string,
+}
 
 @Component({
   selector: 'app-root',
@@ -12,36 +21,109 @@ export class AppComponent implements OnInit {
 
   title = 'esferamascota';
 
-  constructor( @Inject(PLATFORM_ID) private platformId: Object ) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document,
+    ) {}
 
   ngOnInit(): void { this.toggleDisclaimer(); }
 
-  private toggleDisclaimer() {
+  toggleDisclaimer() {
 
+    if (!isPlatformBrowser(this.platformId)) { return; }
+
+    const excludedRoutes = ['cookies', 'politicas', 'terminos'];
+    const currentUrl = window.location.href;
+
+    // Si la ruta actual está excluida no se muestra el popup
+    if (excludedRoutes.some(route => currentUrl.includes(route))) { return; }
+
+    // Si no se aceptaron las cookies se le muestra al usuario el popup
+    const termsAccepted = localStorage.getItem('disclaimer');
+    if (!termsAccepted) {
+      this.showPopupDisclaimer = true;
+      return;
+    }
+
+    // Si no se guardó bien el objeto se le vuelve a mostrar al usuario el popup
+    const acceptedDate = new Date(JSON.parse(termsAccepted).date);
+    if (!acceptedDate) {
+      this.showPopupDisclaimer = true;
+      return;
+    }
+
+    // Si pasaron 6 meses de la aceptación se le vuelve a mostrar al usuario el popup
+    const sixMonthsFromAccepted = new Date(acceptedDate.setMonth(acceptedDate.getMonth() + 6));
+    const currentDate = new Date();
+    this.showPopupDisclaimer = currentDate >= sixMonthsFromAccepted;
+
+    // Acá el usuario ya tiene las cookies aceptadas entonces agrego los scripts
+    if (!this.showPopupDisclaimer) { this.createScripts(); }
+  }
+
+  saveLocalhost() {
     if (isPlatformBrowser(this.platformId))
     {
 
-      const termsAccepted = localStorage.getItem('disclaimer');
-      let shouldShowPopup = true;
+      localStorage.setItem('disclaimer', JSON.stringify({
+        accepted: true,
+        date: new Date()
+      }));
 
-      if (termsAccepted)
-      {
+      // El usuario aceptó las cookies, se agregan los scripts
+      this.createScripts();
+    }
+  }
 
-        const expirationDate = new Date(JSON.parse(termsAccepted).date);
 
-        if (expirationDate)
-        {
-          const expirationMonth = expirationDate.getMonth();
-          const actualMonth = new Date().getMonth();
-          shouldShowPopup = expirationMonth !== actualMonth;
-        }
 
+  createScripts() {
+    if (isPlatformBrowser(this.platformId))
+    {
+      // Agregar Google Analytics y Google Ads scripts
+      const scriptGoogleTagManager: googleScript = {
+        url: 'https://www.googletagmanager.com/gtag/js?id=G-H7X6015BYD',
+        async: true,
+        innerHtml: '',
+        crossOrigin: '',
       }
 
-      if (shouldShowPopup) { this.showPopupDisclaimer = true; }
+      const scriptGooglesyndication: googleScript = {
+        url: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5105612698885287',
+        async: true,
+        innerHtml: '',
+        crossOrigin: 'anonymous',
+      }
 
+      const scriptAnalytics: googleScript = {
+        url: '',
+        async: true,
+        innerHtml: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-H7X6015BYD');
+        `,
+        crossOrigin: '',
+      }
+
+      this.apendScripts(scriptGoogleTagManager);
+      this.apendScripts(scriptGooglesyndication);
+      this.apendScripts(scriptAnalytics);
     }
-
   }
+
+  private apendScripts(scriptObj: googleScript) {
+    const script = document.createElement('script');
+
+    script.src = scriptObj.url;
+    script.async = scriptObj.async;
+    script.innerHTML = scriptObj.innerHtml;
+
+    if (scriptObj.crossOrigin) { script.crossOrigin = scriptObj.crossOrigin; }
+
+    this.document.head.appendChild(script);
+  }
+
 
 }
--- src/app/components/popup-disclaimer/popup-disclaimer.component.ts
+++ src/app/components/popup-disclaimer/popup-disclaimer.component.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, ElementRef, EventEmitter, Inject, Output, PLATFORM_ID, ViewChild, AfterViewInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
@@ -13,7 +13,10 @@ export class PopupDisclaimerComponent implements AfterViewInit {
   @ViewChild('popupDisclaimer') popupDisclaimer: ElementRef<HTMLDivElement> | undefined;
   @Output() disclaimerAccepted: EventEmitter<boolean> = new EventEmitter()
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: any,
+  ) {}
 
 
   ngAfterViewInit(): void {
@@ -21,49 +24,48 @@ export class PopupDisclaimerComponent implements AfterViewInit {
   }
 
   private openPopup() {
-    const popup = this.popupDisclaimer?.nativeElement as HTMLDivElement;
-
-    if (popup)
-    {
-      const content = popup.querySelector('.popup__content') as HTMLDivElement;
-      popup.style.opacity = '1';
-      popup.style.visibility = 'visible';
-      content.style.opacity = '1';
-      content.style.visibility = 'visible';
-      content.style.transform = `translate(-50%, -50%) scale(0.9)`;
-    }
-  }
-
-  saveLocalhost() {
     if (isPlatformBrowser(this.platformId))
     {
 
-      localStorage.setItem('disclaimer', JSON.stringify({
-        accepted: true,
-        date: new Date()
-      }))
+      const popup = this.popupDisclaimer?.nativeElement as HTMLDivElement;
 
-      this.closePopup();
+      if (popup)
+      {
+        const content = popup.querySelector('.popup__content') as HTMLDivElement;
+        popup.style.opacity = '1';
+        popup.style.visibility = 'visible';
+        content.style.opacity = '1';
+        content.style.visibility = 'visible';
+        content.style.transform = `translate(-50%, -50%) scale(0.9)`;
+
+        this.document.body.style.overflow = 'hidden';
+      }
 
     }
   }
 
   closePopup() {
-    const popup = this.popupDisclaimer?.nativeElement as HTMLDivElement;
-
-    if (popup)
+    if (isPlatformBrowser(this.platformId))
     {
-      const content = popup.querySelector('.popup__content') as HTMLDivElement;
-      popup.style.opacity = '0';
-      popup.style.visibility = 'hidden';
-      popup.style.display = 'none';
-      content.style.opacity = '0';
-      content.style.visibility = 'hidden';
-      content.style.display = 'none';
+
+      const popup = this.popupDisclaimer?.nativeElement as HTMLDivElement;
+
+      if (popup)
+      {
+        const content = popup.querySelector('.popup__content') as HTMLDivElement;
+        popup.style.opacity = '0';
+        popup.style.visibility = 'hidden';
+        popup.style.display = 'none';
+        content.style.opacity = '0';
+        content.style.visibility = 'hidden';
+        content.style.display = 'none';
+
+        this.document.body.style.overflow = 'auto';
+      }
+
+      this.disclaimerAccepted.emit(true);
 
     }
-
-    this.disclaimerAccepted.emit(true);
   }
 
 
