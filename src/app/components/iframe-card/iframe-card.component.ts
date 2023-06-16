c72fb790858ba092872a111a211fcaad03bec38b
Totoblak90 <tobias.blaksley@hotmail.com>
2023-06-16 14:41:40 -0300

    added optimizations on iframe card component

--- src/app/components/iframe-card/iframe-card.component.ts
+++ src/app/components/iframe-card/iframe-card.component.ts
@@ -10,12 +10,22 @@ export class IframeCardComponent {
   @Input() videoId: string = '';
   @Input() videoDescription = ''
   videoError: boolean = false;
+  videoLoaded: boolean = false;
 
   get videoUrl() {
     return `https://www.youtube.com/embed/${this.videoId}?autoplay=1&mute=1`;
   }
 
+  get thumbnailUrl() {
+    return `https://img.youtube.com/vi/${this.videoId}/0.jpg`;
+  }
+
   handleError() {
     this.videoError = true;
   }
+
+  loadVideo() {
+    this.videoLoaded = true;
+  }
+
 }
