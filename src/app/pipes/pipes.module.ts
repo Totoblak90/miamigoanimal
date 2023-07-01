98fdd5865a13a98e0f859662d6aef8aa5e85db37
Totoblak90 <tobias.blaksley@hotmail.com>
2023-07-01 17:54:15 -0300

    set a static image on dogs and cats page

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateTempPipe } from './translate-temp.pipe';
 import { SafeUrlPipe } from './safe-url.pipe';
 
 
 
 @NgModule({
   declarations: [
-    TranslateTempPipe,
     SafeUrlPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    TranslateTempPipe,
     SafeUrlPipe
   ]
 })
