eb294f4788e6022ec9ed67ad121e948072b6acd3
Totoblak90 <tobias.blaksley@hotmail.com>
2023-07-11 13:57:00 -0300

    pretefied a file

--- src/app/articles/articles-imports.ts
+++ src/app/articles/articles-imports.ts
@@ -1,7 +1,13 @@
-import { RouterModule } from "@angular/router";
-import { ArticleSkeletonComponent } from "../components/article-skeleton/article-skeleton.component";
-import { HeaderComponent } from "../components/header/header.component";
-import { IframeCardComponent } from "../components/iframe-card/iframe-card.component";
-import { IndexComponent } from "../components/index/index.component";
+import { RouterModule } from '@angular/router';
+import { ArticleSkeletonComponent } from '../components/article-skeleton/article-skeleton.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { IframeCardComponent } from '../components/iframe-card/iframe-card.component';
+import { IndexComponent } from '../components/index/index.component';
 
-export const ARTICLES_IMPORTS =   [IndexComponent, ArticleSkeletonComponent, HeaderComponent, IframeCardComponent, RouterModule]
+export const ARTICLES_IMPORTS = [
+   IndexComponent,
+   ArticleSkeletonComponent,
+   HeaderComponent,
+   IframeCardComponent,
+   RouterModule,
+];
